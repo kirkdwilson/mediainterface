c7a3c9cd472b60e410aee4acc0b4ceac46a0ce8b
Codemis <johnathan@pulos.com>
2021-08-11 15:17:52 -0700

    Added an episode model.

---
+++ src/models/episode.spec.ts
@@ -0,0 +1,40 @@
+import {} from 'jasmine';
+import { Episode } from './episode';
+
+describe('Episode', () => {
+
+  describe('imagePath', () => {
+
+    it('should contain the correct values', () => {
+      const episode = new Episode(
+        'A show about tuna!',
+        'tuna.mp4',
+        'tuna.jpg',
+        'video',
+        'A Fishy Tale',
+        'ch'
+      );
+      expect(episode.imagePath).toContain('/ch/');
+      expect(episode.imagePath).toContain('tuna.jpg');
+    });
+
+  });
+
+  describe('filePath', () => {
+
+    it('should contain the correct values', () => {
+      const episode = new Episode(
+        'A show about boats!',
+        'boat.mp4',
+        'boat.jpg',
+        'video',
+        'A Tuna Boat',
+        'zh'
+      );
+      expect(episode.filePath).toContain('/zh/');
+      expect(episode.filePath).toContain('boat.mp4');
+    });
+
+  });
+
+});
--- src/models/media.ts
+++ src/models/media.ts
@@ -1,10 +1,29 @@
 import { environment } from '@env';
 import { Category } from './category';
+import { Episode } from './episode';
 
 /**
  * A model for a piece of Media
  */
 export class Media {
+  /**
+   * A list of episodes
+   */
+  public episodes: Array<Episode> = [];
+
+  /**
+   * Build the class
+   *
+   * @param categories        An array of categories
+   * @param desc              The description
+   * @param image             The image file name
+   * @param mediaType         The type of media
+   * @param slug              A slug to identify the media
+   * @param title             The title
+   * @param language='en'     The current language
+   * @param recommended=false Is it recommended?
+   * @param tags              An array of tags
+   */
   constructor(
     public categories: Array<Category>,
     public desc: string,
@@ -24,6 +43,16 @@ export class Media {
    */
   get imagePath(): string {
     return `${environment.assetPath.replace('{LANG}', this.language)}images/${this.image}`;
+  }
+
+  /**
+   * Add an episode to the media.
+   *
+   * @param  episode The episode to add
+   * @return         void
+   */
+  addEpisode(episode: Episode) {
+    this.episodes.push(episode);
   }
 
 }
