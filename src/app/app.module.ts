32b1170e32c0bea133925d0030299d1e89ecdd57
Codemis <johnathan@pulos.com>
2021-08-12 16:50:13 -0700

    Removed unused libraries.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from '@app/app.component';
 
@@ -21,8 +19,6 @@ import { MyApp } from '@app/app.component';
     MyApp,
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
   ]
 })
