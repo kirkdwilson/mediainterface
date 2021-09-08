3a2910f950823a8a26a58b6e6f03e608b822be35
Codemis <johnathan@pulos.com>
2021-09-07 18:13:30 -0700

    We can now display an ebook and change pages.

--- src/pages/epub-viewer/epub-viewer.ts
+++ src/pages/epub-viewer/epub-viewer.ts
@@ -1,9 +1,15 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { Book, Rendition } from 'epubjs';
 import { take } from 'rxjs/operators/take';
 import { DownloadFileProvider } from '@providers/download-file/download-file';
-import { EpubViewerItem } from './epub-viewer-item';
+import { EpubViewerItem } from './epub-viewer-item.interface';
 import { NavParamsDataStoreProvider } from '@providers/nav-params-data-store/nav-params-data-store';
+
+/**
+ * ePubjs is included in index.html
+ */
+// declare var ePub: any;
 
 /**
  * An epub viewer.
@@ -17,6 +23,21 @@ import { NavParamsDataStoreProvider } from '@providers/nav-params-data-store/nav
   templateUrl: 'epub-viewer.html',
 })
 export class EpubViewerPage {
+
+  /**
+   * The book we are viewing
+   */
+  book: any = null;
+
+  /**
+   * The rendition of the book
+   */
+  rendition: any = null;
+
+  /**
+   * The current page we are viewing
+   */
+  currentPage: number = 1;
 
   /**
    * A reference to the download link
@@ -87,10 +108,6 @@ export class EpubViewerPage {
     });
   }
 
-  loadFile() {
-    console.log('Loading...');
-  }
-
   /**
    * Go back to the previous page
    *
@@ -118,6 +135,43 @@ export class EpubViewerPage {
         animate: true,
       });
     }
+  }
+
+  /**
+   * Load the file
+   *
+   * @return void
+   */
+  loadFile() {
+    this.book = new Book(this.item.url);
+    this.rendition = this.book.renderTo('book', { width: '100%', height: '100%' });
+    this.rendition.display();
+    this.rendition.on('relocated', (location) => {
+      console.log(location);
+    });
+  }
+
+  /**
+   * Go to the previous page.
+   *
+   * @return void
+   */
+  prev() {
+    if (this.currentPage === 1) {
+        return;
+    }
+    this.currentPage -= 1;
+    this.rendition.prev();
+  }
+
+  /**
+   * Go to the next page.
+   *
+   * @return void
+   */
+  next() {
+    this.currentPage += 1;
+    this.rendition.next();
   }
 
 }
