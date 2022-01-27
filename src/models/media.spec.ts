ab7f83698dd5f2babc775dca117baf5ef8db64ff
Codemis <johnathan@pulos.com>
2022-01-27 14:10:22 -0800

    Updated filePath and created a downloadPath for both Media and Episode models.  On Media replaced order with slug.

--- src/models/media.spec.ts
+++ src/models/media.spec.ts
@@ -28,24 +28,115 @@ describe('Media', () => {
 
   });
 
-  describe('filePath', () => {
+  describe('categoryList', () => {
 
-    it('should contain the correct values', () => {
+    it('should return the list', () => {
       const media = new Media(
-        [new Category('Free')],
-        'A free show',
-        'free-show.jpg',
+        [new Category('Unique'), new Category('specials'), new Category('original')],
+        'A unique experience',
+        'unique-experience.jpg',
         'video',
-        'really-free',
-        'Really Free',
-        'really-free.mp4',
-        'ch',
-        'video/mp4',
-        false,
-        ['free']
+        'unique-experience',
+        'Unique Experience'
       );
-      expect(media.filePath).toContain('/ch/');
-      expect(media.filePath).toContain('really-free.mp4');
+      expect(media.categoryList).toEqual('Original, Specials, Unique');
+    });
+
+  });
+
+  describe('downloadPath', () =>  {
+
+    it('should return the correct path for standard file', () => {
+      const media = new Media(
+        [new Category('specials'), new Category('original')],
+        'A challenging task',
+        'challenging-task.jpg',
+        'video',
+        'challenging-task',
+        'Challenging Task',
+        'challenging-task.mp4',
+        'zh'
+      );
+      expect(media.downloadPath).toEqual('assets/content/zh/media/challenging-task.mp4');
+    });
+
+    it('should return the correct path for html files', () => {
+      const media = new Media(
+        [new Category('specials'), new Category('original')],
+        'my dog webpage',
+        'my-dog-webpage.jpg',
+        'html',
+        'my-dog-webpage',
+        'My Dog Webpage',
+        '',
+        'en'
+      );
+      expect(media.downloadPath).toEqual('assets/content/en/html/my-dog-webpage.zip');
+    });
+
+    it('should return the correct path for h5p files', () => {
+      const media = new Media(
+        [new Category('specials'), new Category('original')],
+        'cat care',
+        'cat-care.jpg',
+        'h5p',
+        'cat-care',
+        'Cat Care',
+        '',
+        'pt'
+      );
+      expect(media.downloadPath).toEqual('assets/content/pt/media/cat-care.h5p');
+    });
+
+  });
+
+  describe('filePath', () =>  {
+
+    it('should return the correct path for standard file', () => {
+      const media = new Media(
+        [new Category('specials'), new Category('original')],
+        'A challenging task',
+        'challenging-task.jpg',
+        'video',
+        'challenging-task',
+        'Challenging Task',
+        'challenging-task.mp4',
+        'zh'
+      );
+      expect(media.filePath).toContain('/zh/');
+      expect(media.filePath).toContain('media/challenging-task.mp4');
+    });
+
+    it('should return the correct path for html files', () => {
+      const media = new Media(
+        [new Category('specials'), new Category('original')],
+        'my dog webpage',
+        'my-dog-webpage.jpg',
+        'html',
+        'my-dog-webpage',
+        'My Dog Webpage',
+        '',
+        'en'
+      );
+      expect(media.filePath).toContain('/en/');
+      expect(media.filePath).toContain('html/my-dog-webpage');
+      expect(media.filePath).not.toContain('.zip');
+    });
+
+    it('should return the correct path for h5p files', () => {
+      const media = new Media(
+        [new Category('specials'), new Category('original')],
+        'cat care',
+        'cat-care.jpg',
+        'h5p',
+        'cat-care',
+        'Cat Care',
+        '',
+        'pt'
+      );
+      expect(media.filePath).toContain('/pt/');
+      expect(media.filePath).toContain('media/cat-care');
+      expect(media.filePath).not.toContain('.h5p');
     });
 
     it('should return empty if no file name set', () => {
@@ -63,22 +154,6 @@ describe('Media', () => {
         ['free']
       );
       expect(media.filePath).toEqual('');
-    });
-
-  });
-
-  describe('categoryList', () => {
-
-    it('should return the list', () => {
-      const media = new Media(
-        [new Category('Unique'), new Category('specials'), new Category('original')],
-        'A unique experience',
-        'unique-experience.jpg',
-        'video',
-        'unique-experience',
-        'Unique Experience'
-      );
-      expect(media.categoryList).toEqual('Original, Specials, Unique');
     });
 
   });
